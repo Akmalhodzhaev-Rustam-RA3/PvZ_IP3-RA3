3687da41daa795411a313b56ad8e88e9b56c330c
PML30\cgsg245 <rustam.rein.akmalhodzaev@gmail.com>
2026-06-12 11:44:52 +0300

    Create project

---
+++ PvZ/src/client/main.js
@@ -0,0 +1,25 @@
+import { io } from "socket.io-client";
+
+async function main() {
+    const socket = io();
+
+    socket.on("connect", () => {
+        console.log(socket.id);
+        socket.emit("messageToServer", `Hello`);
+        socket.on("messageFromServer", function (msg) {
+            const messages = document.getElementById("main")
+            const item = document.createElement('li');
+            item.textContent = msg;
+            messages.appendChild(item);
+            console.log(msg);
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log(socket.id); // undefined
+    });
+}
+
+window.addEventListener("load", () => {
+    main();
+});
